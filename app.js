1783fe17158e06514e5f0cfc6a1930efe4be5c3f
lys7375 <yushuolu7375@gmail.com>
2019-02-06 21:35:25 -0800

    modified config for deployment

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 let http = require('http');
 let date = new Date();
 date.toLocaleString();
-
+const PORT = process.env.PORT || 3000;
 http.createServer(function (request, responce)
 {
     responce.writeHead(200, {'Content-type':'text/plain'});
     responce.write("Response's coming from server...\n");
     responce.write("Current Time" + date);
     responce.end();
-}).listen(9000);
+}).listen(PORT);
 console.log('listening...');
